245ca4219370105d41c0055ecb278348aef6d280
fedpre <fed.pregnolato@gmail.com>
2023-02-23 08:19:21 +0100

    first commit

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,41 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by Federico Pregnolato on 21/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavander
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavander, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
+
+
